f22558f0482b413325cb7b416de9da21d72a5761
forgocode <forgocode@163.com>
2023-06-01 04:16:24 +0000

    update mapping config

--- neovim/lua/config/mapping.lua
+++ neovim/lua/config/mapping.lua
@@ -20,10 +20,4 @@ vim.keymapping.smap("n", "sh", "<cmd>sp<CR>", vim.keymapping.opts)
 vim.keymapping.smap("n", "sc", "<C-w>c", vim.keymapping.opts)
 vim.keymapping.smap("n", "so", "<C-w>o", vim.keymapping.opts)
 
--- 分屏模式的移动
-vim.keymapping.smap("n", "<A-k>", "<C-w>k", vim.keymapping.opts)
-vim.keymapping.smap("n", "<A-j>", "<C-w>j", vim.keymapping.opts)
-vim.keymapping.smap("n", "<A-h>", "<C-w>h", vim.keymapping.opts)
-vim.keymapping.smap("n", "<A-l>", "<C-w>l", vim.keymapping.opts)
 
-vim.keymapping.smap("n", "gvG", "ggvG=", vim.keymapping.opts)
--- neovim/lua/plugin/config/bufferline.lua
+++ neovim/lua/plugin/config/bufferline.lua
@@ -11,8 +11,8 @@ require("bufferline").setup({
 })
 
 -- 关闭当前bufferline
-vim.keymapping.smap("n", "<C-q>", "<cmd>Bdelete!<CR>", vim.keymapping.opts)
+vim.keymapping.smap("n", "<A-q>", "<cmd>Bdelete!<CR>", vim.keymapping.opts)
 -- 切换左侧缓冲区
-vim.keymapping.smap("n", "<C-h>", "<cmd>BufferLineCyclePrev<CR>", vim.keymapping.opts)
+vim.keymapping.smap("n", "<A-h>", "<cmd>BufferLineCyclePrev<CR>", vim.keymapping.opts)
 -- 切换右侧缓冲区
-vim.keymapping.smap("n", "<C-l>", "<cmd>BufferLineCycleNext<CR>", vim.keymapping.opts)
+vim.keymapping.smap("n", "<A-l>", "<cmd>BufferLineCycleNext<CR>", vim.keymapping.opts)
