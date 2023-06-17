6a2627944d35b28710a746f69374e13f42733b8e
forgocode <forgocode@163.com>
2023-06-17 09:01:54 +0800

    rm commit history

---
+++ neovim/lua/plugin/config/mason-lspconfig-nvim.lua
@@ -0,0 +1,12 @@
+--**********************************************************
+--* Author           : forgocode
+--* Email            : forgocode@163.com
+--* Github           : https://github.com/forgocode
+--* Create Time      : 2023-06-08 13:11
+--* FileName         : mason-lspconfig.lua
+--* Description      :
+--**********************************************************
+
+require("mason-lspconfig").setup({
+	ensure_installed = { "gopls", "lua_ls", "rust_analyzer", "bashls" },
+})
